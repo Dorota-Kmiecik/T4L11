9c639292614e6c6e09dd3fe75ca31ca072f74991
Dorota Kmiecik <dorota.kmiecik87@gmail.com>
2023-05-24 22:13:43 +0200

    Adding new files

---
+++ Zadanie domowe Tydzien 4 Lekcja 11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_domowe_Tydzien_4_Lekcja_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                try
+                {
+                
+                    Console.WriteLine("Wprowadz jakas liczbe calkowita:");
+                    var number = int.Parse(Console.ReadLine());
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine("Wprowadziles liczbe parzysta");
+                    }
+                   
+                    else
+                    {
+                        Console.WriteLine("Wprowadziles liczbe nieparzysta");
+                    }
+                
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message); ;
+                }
+                finally
+                { 
+                    Console.ReadLine(); 
+                }
+               
+            
+            
+        }
+    }
+}
