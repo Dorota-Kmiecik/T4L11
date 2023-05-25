62cb107ef9191a55e138db6f42ea0f716cb5e66c
Dorota Kmiecik <dorota.kmiecik87@gmail.com>
2023-05-25 18:39:26 +0200

    Removing spaces

--- Zadanie domowe Tydzien 4 Lekcja 11/Program.cs
+++ Zadanie domowe Tydzien 4 Lekcja 11/Program.cs
@@ -10,35 +10,28 @@ namespace Zadanie_domowe_Tydzien_4_Lekcja_11
     {
         static void Main(string[] args)
         {
-           
-                try
+            try
+            {
+                Console.WriteLine("Wprowadz jakas liczbe calkowita:");
+                var number = int.Parse(Console.ReadLine());
+                if (number % 2 == 0)
                 {
-                
-                    Console.WriteLine("Wprowadz jakas liczbe calkowita:");
-                    var number = int.Parse(Console.ReadLine());
-                    if (number % 2 == 0)
-                    {
-                        Console.WriteLine("Wprowadziles liczbe parzysta");
-                    }
-                   
-                    else
-                    {
-                        Console.WriteLine("Wprowadziles liczbe nieparzysta");
-                    }
-                
+                    Console.WriteLine("Wprowadziles liczbe parzysta");
                 }
-                catch (Exception ex)
+                else
                 {
-
-                    Console.WriteLine(ex.Message); ;
+                    Console.WriteLine("Wprowadziles liczbe nieparzysta");
                 }
-                finally
-                { 
-                    Console.ReadLine(); 
-                }
-               
-            
-            
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); ;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
+
